2b7cb455b0e3cb413ff7371b82fbbacc88801bac
lucas55i <lucas_55i@outlook.com>
2022-04-24 11:54:15 -0300

    definição de swict case

--- switch/ex001.js
+++ switch/ex001.js
@@ -16,88 +16,64 @@ if (info === "newEvent") {
   console.log(type);
 }
 
-if (isSuccess === true) {
-  if (option === "notification") {
-    let notify = "Notificação ";
-    type = "success";
-    canaisComunicacao.push(notify);
-    console.log(canaisComunicacao);
-    console.log(type);
-  }
-  if (option === "automation") {
-    let auto = "Automação ";
-    type = "success";
-    automations.push(auto);
-    console.log(automations);
-    console.log(type);
-  }
-  if (option === "ticket") {
-    let auto = "Ticket ";
-    type = "success";
-    automations.push(auto);
-    console.log(automations);
-    console.log(type);
-  }
-}
-
 // #################################################
 
 // por switch case, preciso ver se esse formato é melhor para fazer o que preciso
 
-// switch (info) {
-//   case "newEvent":
-//     let newE = "Novo evento";
-//     newEvents.push(newE);
-//     console.log(newEvents);
-//     type = "info";
-//     console.log(type);
-//     break;
-//   case "automation":
-//     if (isSuccess === true) {
-//       let auto = "Automação ";
-//       type = "success";
-//       automations.push(auto);
-//       console.log(automations);
-//       console.log(type);
-//     } else {
-//       let auto = "Automação ";
-//       type = "error";
-//       automations.push(auto);
-//       console.log(automations);
-//       console.log(type);
-//     }
-//     break;
-//   case "notification":
-//     if (isSuccess === true) {
-//       let notify = "Notificação ";
-//       type = "success";
-//       canaisComunicacao.push(notify);
-//       console.log(canaisComunicacao);
-//       console.log(type);
-//     } else {
-//       let notify = "Notificação ";
-//       type = "error";
-//       canaisComunicacao.push(notify);
-//       console.log(canaisComunicacao);
-//       console.log(type);
-//     }
-//     break;
-//   case "ticket":
-//     if (isSuccess === true) {
-//       let ticket = "Tickets";
-//       type = "success";
-//       tickets.push(ticket);
-//       console.log(tickets);
-//       console.log(type);
-//     } else {
-//       let ticket = "Tickets";
-//       type = "error";
-//       tickets.push(ticket);
-//       console.log(tickets);
-//       console.log(type);
-//     }
-//     break;
+switch (info) {
+  case "newEvent":
+    let newE = "Novo evento";
+    newEvents.push(newE);
+    console.log(newEvents);
+    type = "info";
+    console.log(type);
+    break;
+  case "automation":
+    if (isSuccess === true) {
+      let auto = "Automação ";
+      type = "success";
+      automations.push(auto);
+      console.log(automations);
+      console.log(type);
+    } else {
+      let auto = "Automação ";
+      type = "error";
+      automations.push(auto);
+      console.log(automations);
+      console.log(type);
+    }
+    break;
+  case "notification":
+    if (isSuccess === true) {
+      let notify = "Notificação ";
+      type = "success";
+      canaisComunicacao.push(notify);
+      console.log(canaisComunicacao);
+      console.log(type);
+    } else {
+      let notify = "Notificação ";
+      type = "error";
+      canaisComunicacao.push(notify);
+      console.log(canaisComunicacao);
+      console.log(type);
+    }
+    break;
+  case "ticket":
+    if (isSuccess === true) {
+      let ticket = "Tickets";
+      type = "success";
+      tickets.push(ticket);
+      console.log(tickets);
+      console.log(type);
+    } else {
+      let ticket = "Tickets";
+      type = "error";
+      tickets.push(ticket);
+      console.log(tickets);
+      console.log(type);
+    }
+    break;
 
-//   default:
-//     console.log("Sem notificação selecionada!");
-// }
+  default:
+    console.log("Sem notificação selecionada!");
+}
